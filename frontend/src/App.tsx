0055642c256a63a02d432ea748538ca5165b358e
macololidoll <makomakopg@gmail.com>
2020-06-10 16:20:42 +0900

    feat: Add TailwindCSS.
    TailwindCSSの導入。
    導入に伴うyarn scriptなどの追加。

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './tailwind.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img src={logo} className="App-logo mt-24" alt="logo" />
+        <p className="text-blue-900">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link text-blue-500"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
